3ebc1e73457a5ae61588eebed3c99653998bf262
MalaikaNest Ops <ops@malaikanest.local>
2026-03-26 00:27:42 +0300

    Fix login: make store layout client component with providers

--- frontend/src/app/(store)/layout.tsx
+++ frontend/src/app/(store)/layout.tsx
@@ -1,4 +1,7 @@
-import { Providers } from '@/lib/providers'
+'use client'
+
+import { CartProvider } from '@/lib/cartContext'
+import { AuthProvider } from '@/lib/authContext'
 import Navbar from '@/components/layout/Navbar'
 import TrustBar from '@/components/layout/TrustBar'
 import Footer from '@/components/layout/Footer'
@@ -7,15 +10,17 @@ import Chatbot from '@/components/Chatbot'
 
 export default function StoreLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Providers>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1">{children}</main>
-        <TrustBar />
-        <Footer />
-      </div>
-      <Chatbot />
-      <ToastContainer />
-    </Providers>
+    <AuthProvider>
+      <CartProvider>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <main className="flex-1">{children}</main>
+          <TrustBar />
+          <Footer />
+        </div>
+        <Chatbot />
+        <ToastContainer />
+      </CartProvider>
+    </AuthProvider>
   )
 }
